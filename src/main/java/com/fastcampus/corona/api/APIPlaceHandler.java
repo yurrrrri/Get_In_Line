576ca74bdd71fffd80d0cc0bbea26d37591324b0
yurison <yu_ri_son@naver.com>
2023-07-19 22:56:33 +0900

    함수형 프로그래밍 연습

---
+++ src/main/java/com/fastcampus/corona/api/APIPlaceHandler.java
@@ -0,0 +1,40 @@
+package com.fastcampus.corona.api;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.function.ServerRequest;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+import java.util.List;
+
+import static org.springframework.web.servlet.function.ServerResponse.created;
+import static org.springframework.web.servlet.function.ServerResponse.ok;
+
+/**
+ * 메서드 구현 시 HandlerFunction을 구현하고 메서드 오버라이딩 한 후에 삭제시켜주면 쉽다.
+ */
+@Component
+public class APIPlaceHandler {
+
+    public ServerResponse getPlaces(ServerRequest request) {
+        return ok().body(List.of("place1", "place2"));
+    }
+
+    // TODO: 1은 구현할 때 제대로 넣기
+    public ServerResponse createPlace(ServerRequest request) {
+        return created(URI.create("/api/places/1")).body(true);
+    }
+
+    public ServerResponse getPlace(ServerRequest request) {
+        return ok().body(List.of("place " + request.pathVariable("placeId")));
+    }
+
+    public ServerResponse modifyPlace(ServerRequest request) {
+        return ok().body(true);
+    }
+
+    public ServerResponse removePlace(ServerRequest request) {
+        return ok().body(true);
+    }
+
+}
---
+++ src/main/java/com/fastcampus/corona/api/APIPlaceRouter.java
@@ -0,0 +1,40 @@
+package com.fastcampus.corona.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RequestPredicates.path;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+
+/**
+ * 함수형 프로그래밍
+ * APIPlaceController와 충돌하므로 실행 시 컨트롤러나 라우터의 어노테이션을 주석 처리한다.
+ */
+@Configuration
+public class APIPlaceRouter {
+
+//    @Bean
+//    public RouterFunction<ServerResponse> placeRouter() {
+//        return route().nest(path("/api/places"), builder -> builder
+//                .GET("", req -> ServerResponse.ok().body(List.of("place1", "place2")))
+//                .POST("", req -> ServerResponse.ok().body(true))
+//                .GET("/{placeId}", req -> ServerResponse.ok().body("place " + req.pathVariable("placeId")))
+//                .PUT("/{placeId}", req -> ServerResponse.ok().body(true))
+//                .DELETE("/{placeId}", req -> ServerResponse.ok().body(true))
+//        ).build();
+//    }
+
+    @Bean
+    public RouterFunction<ServerResponse> placeRouter(APIPlaceHandler apiPlaceHandler) {
+        return route().nest(path("/api/places"), builder -> builder
+                .GET("", apiPlaceHandler::getPlaces)
+                .POST("", apiPlaceHandler::createPlace)
+                .GET("/{placeId}", apiPlaceHandler::getPlace)
+                .PUT("/{placeId}", apiPlaceHandler::modifyPlace)
+                .DELETE("/{placeId}", apiPlaceHandler::removePlace)
+        ).build();
+    }
+
+}
