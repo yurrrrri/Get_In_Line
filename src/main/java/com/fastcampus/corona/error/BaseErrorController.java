81b24b56eea4cf4d54b83f194900f58b4c3dedd9
yurison <yu_ri_son@naver.com>
2023-07-21 14:12:38 +0900

    refactor: 예외 컨트롤러 메서드를 분리

---
+++ src/main/java/com/fastcampus/corona/error/BaseErrorController.java
@@ -0,0 +1,15 @@
+package com.fastcampus.corona.error;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BaseErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String error() {
+        return "error";
+    }
+
+}
