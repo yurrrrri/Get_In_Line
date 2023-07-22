fb462296702fcf30443814040963359c70c151d5
yurison <yu_ri_son@naver.com>
2023-07-22 15:51:05 +0900

    test: 기본 테스트 메서드 구현

---
+++ src/test/java/com/fastcampus/corona/controller/BaseControllerTest.java
@@ -0,0 +1,36 @@
+package com.fastcampus.corona.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class BaseControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @DisplayName("[view] [GET] 기본 페이지 요청")
+    @Test
+    void givenNothing_whenRequestingRootPage_thenReturnsIndexPage() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(content().string(containsString("This is default page.")))
+                .andExpect(view().name("index"))
+                .andDo(print());
+    }
+}
