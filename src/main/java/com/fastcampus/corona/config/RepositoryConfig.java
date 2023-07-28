4401a423e56666f7442f2cfa825a1ee5bbce2ba8
yurison <yu_ri_son@naver.com>
2023-07-28 23:21:09 +0900

    feat: EventRepository

---
+++ src/main/java/com/fastcampus/corona/config/RepositoryConfig.java
@@ -0,0 +1,15 @@
+package com.fastcampus.corona.config;
+
+import com.fastcampus.corona.repository.EventRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RepositoryConfig {
+
+    @Bean
+    public EventRepository eventRepository() {
+        return new EventRepository() {};
+    }
+
+}
--- src/main/java/com/fastcampus/corona/repository/EventRepository.java
+++ src/main/java/com/fastcampus/corona/repository/EventRepository.java
@@ -9,16 +9,21 @@ import java.util.Optional;
 
 public interface EventRepository {
 
+    // TODO : 인스턴스 생성 편의를 위해 임시로 default 사용, Repository layer 구현이 완료되면 삭제
     default List<EventDto> findEvents(
             Long placeId,
             String eventName,
             EventStatus eventStatus,
             LocalDateTime eventStartDatetime,
             LocalDateTime eventEndDatetime
-    ) { return null; }
+    ) { return List.of(); }
+
     default Optional<EventDto> findEvent(Long eventId) { return Optional.empty(); }
+
     default boolean insertEvent(EventDto eventDTO) { return false; }
+
     default boolean updateEvent(Long eventId, EventDto dto) { return false; }
+
     default boolean deleteEvent(Long eventId) { return false; }
 
 }
