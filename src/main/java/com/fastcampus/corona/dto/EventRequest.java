5c5d810b37caa3a72b79dd0dae21d92949db0881
yurison <yu_ri_son@naver.com>
2023-07-28 21:06:15 +0900

    feat: APIEventControllerTest

---
+++ src/main/java/com/fastcampus/corona/dto/EventRequest.java
@@ -0,0 +1,38 @@
+package com.fastcampus.corona.dto;
+
+import com.fastcampus.corona.constant.EventStatus;
+
+import java.time.LocalDateTime;
+
+public record EventRequest(
+        Long placeId,
+        String eventName,
+        EventStatus eventStatus,
+        LocalDateTime eventStartDatetime,
+        LocalDateTime eventEndDatetime,
+        Integer currentNumberOfPeople,
+        Integer capacity,
+        String memo
+) {
+    public static EventRequest of(
+            Long placeId,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDatetime,
+            LocalDateTime eventEndDatetime,
+            Integer currentNumberOfPeople,
+            Integer capacity,
+            String memo
+    ) {
+        return new EventRequest(
+                placeId,
+                eventName,
+                eventStatus,
+                eventStartDatetime,
+                eventEndDatetime,
+                currentNumberOfPeople,
+                capacity,
+                memo
+        );
+    }
+}
---
+++ src/main/java/com/fastcampus/corona/dto/EventResponse.java
@@ -0,0 +1,38 @@
+package com.fastcampus.corona.dto;
+
+import com.fastcampus.corona.constant.EventStatus;
+
+import java.time.LocalDateTime;
+
+public record EventResponse(
+        Long placeId,
+        String eventName,
+        EventStatus eventStatus,
+        LocalDateTime eventStartDatetime,
+        LocalDateTime eventEndDatetime,
+        Integer currentNumberOfPeople,
+        Integer capacity,
+        String memo
+) {
+    public static EventResponse of(
+            Long placeId,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDatetime,
+            LocalDateTime eventEndDatetime,
+            Integer currentNumberOfPeople,
+            Integer capacity,
+            String memo
+    ) {
+        return new EventResponse(
+                placeId,
+                eventName,
+                eventStatus,
+                eventStartDatetime,
+                eventEndDatetime,
+                currentNumberOfPeople,
+                capacity,
+                memo
+        );
+    }
+}
