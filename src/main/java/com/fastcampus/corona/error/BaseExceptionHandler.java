baca1c251f93ffeeb47f65e545e5f12bdadd6c89
yurison <yu_ri_son@naver.com>
2023-11-22 18:20:50 +0900

    refactor: ExceptionHandlers

--- src/main/java/com/fastcampus/corona/error/BaseExceptionHandler.java
+++ src/main/java/com/fastcampus/corona/error/BaseExceptionHandler.java
@@ -2,7 +2,6 @@ package com.fastcampus.corona.error;
 
 import com.fastcampus.corona.constant.ErrorCode;
 import com.fastcampus.corona.exception.GeneralException;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,35 +14,30 @@ public class BaseExceptionHandler {
     @ExceptionHandler
     public ModelAndView general(GeneralException e) {
         ErrorCode errorCode = e.getErrorCode();
-        HttpStatus status = errorCode.isClientSideError() ?
-                HttpStatus.BAD_REQUEST :
-                HttpStatus.INTERNAL_SERVER_ERROR;
 
         return new ModelAndView(
                 "error",
                 Map.of(
-                        "statusCode", status.value(),
+                        "statusCode", errorCode.getHttpStatus().value(),
                         "errorCode", errorCode,
                         "message", errorCode.getMessage()
                 ),
-                status
+                errorCode.getHttpStatus()
         );
     }
 
     @ExceptionHandler
     public ModelAndView exception(Exception e) {
         ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
         return new ModelAndView(
                 "error",
                 Map.of(
-                        "statusCode", status.value(),
+                        "statusCode", errorCode.getHttpStatus().value(),
                         "errorCode", errorCode,
                         "message", errorCode.getMessage(e)
                 ),
-                status
+                errorCode.getHttpStatus()
         );
     }
-
 }
