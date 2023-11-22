86265052249685bb7a39da933bcf58f8e6885b0c
yurison <yu_ri_son@naver.com>
2023-11-22 18:17:02 +0900

    refactor : ErrorCode to have HttpStatus

--- src/main/java/com/fastcampus/corona/constant/ErrorCode.java
+++ src/main/java/com/fastcampus/corona/constant/ErrorCode.java
@@ -1,8 +1,11 @@
 package com.fastcampus.corona.constant;
 
+import com.fastcampus.corona.exception.GeneralException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.function.Predicate;
 
@@ -10,23 +13,39 @@ import java.util.function.Predicate;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    OK(0, ErrorCategory.NORMAL, "OK"),
+    OK(0, HttpStatus.OK, "Ok"),
 
-    BAD_REQUEST(10000, ErrorCategory.CLIENT_SIDE, "Bad request"),
-    SPRING_BAD_REQUEST(10001, ErrorCategory.CLIENT_SIDE, "Spring-detected bad request"),
-    VALIDATION_ERROR(10002, ErrorCategory.CLIENT_SIDE, "Validation error"),
-    NOT_FOUND(10003, ErrorCategory.CLIENT_SIDE, "Requested resource is not found"),
+    BAD_REQUEST(10000, HttpStatus.BAD_REQUEST, "Bad request"),
+    SPRING_BAD_REQUEST(10001, HttpStatus.BAD_REQUEST, "Spring-detected bad request"),
+    VALIDATION_ERROR(10002, HttpStatus.BAD_REQUEST, "Validation error"),
+    NOT_FOUND(10003, HttpStatus.BAD_REQUEST, "Requested resource is not found"),
 
-    INTERNAL_ERROR(20000, ErrorCategory.SERVER_SIDE, "Internal error"),
-    SPRING_INTERNAL_ERROR(20001, ErrorCategory.SERVER_SIDE, "Spring-detected internal error"),
-    DATA_ACCESS_ERROR(20002, ErrorCategory.SERVER_SIDE, "Data access error");
+    INTERNAL_ERROR(20000, HttpStatus.INTERNAL_SERVER_ERROR, "Internal error"),
+    SPRING_INTERNAL_ERROR(20001, HttpStatus.INTERNAL_SERVER_ERROR, "Spring-detected internal error"),
+    DATA_ACCESS_ERROR(20002, HttpStatus.INTERNAL_SERVER_ERROR, "Data access error");
 
     private final Integer code;
-    private final ErrorCategory errorCategory;
+    private final HttpStatus httpStatus;
     private final String message;
 
-    public String getMessage(Exception e) {
-        return getMessage(this.getMessage() + " - " + e.getMessage());
+
+    public static ErrorCode valueOf(HttpStatus httpStatus) {
+        if (httpStatus == null) {
+            throw new GeneralException("HttpStatus is null.");
+        }
+
+        return Arrays.stream(values())
+                .filter(errorCode -> errorCode.getHttpStatus() == httpStatus)
+                .findFirst()
+                .orElseGet(() -> {
+                    if (httpStatus.is4xxClientError()) {
+                        return ErrorCode.BAD_REQUEST;
+                    } else if (httpStatus.is5xxServerError()) {
+                        return ErrorCode.INTERNAL_ERROR;
+                    } else {
+                        return ErrorCode.OK;
+                    }
+                });
     }
 
     public String getMessage(Throwable e) {
@@ -36,25 +55,11 @@ public enum ErrorCode {
     public String getMessage(String message) {
         return Optional.ofNullable(message)
                 .filter(Predicate.not(String::isBlank))
-                .orElse(getMessage());
-    }
-
-    public boolean isClientSideError() {
-        return this.getErrorCategory() == ErrorCategory.CLIENT_SIDE;
-    }
-
-    public boolean isServerSideError() {
-        return this.getErrorCategory() == ErrorCategory.SERVER_SIDE;
+                .orElse(this.getMessage());
     }
 
     @Override
     public String toString() {
-        return String.format("%s (%d)", name(), this.getCode());
+        return String.format("%s (%d)", this.name(), this.getCode());
     }
-
-
-    public enum ErrorCategory {
-        NORMAL, CLIENT_SIDE, SERVER_SIDE
-    }
-
 }
--- src/test/java/com/fastcampus/corona/constant/ErrorCodeTest.java
+++ src/test/java/com/fastcampus/corona/constant/ErrorCodeTest.java
@@ -1,35 +1,39 @@
 package com.fastcampus.corona.constant;
 
+import com.fastcampus.corona.exception.GeneralException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.http.HttpStatus;
 
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+@DisplayName("에러 처리 - 에러 코드")
 class ErrorCodeTest {
 
-    @ParameterizedTest(name = "[{index}] {0} ===> {1}")
+    @DisplayName("예외를 받으면, 예외 메시지가 포함된 메시지 출력")
     @MethodSource
-    @DisplayName("예외를 받으면, 예외 메시지가 포함된 메시지가 출력된다.")
+    @ParameterizedTest(name = "[{index}] {0} ===> {1}")
     void givenExceptionWithMessage_whenGettingMessage_thenReturnsMessage(ErrorCode sut, String expected) {
-        // given
+        // Given
         Exception e = new Exception("This is test message.");
 
-        // when
+        // When
         String actual = sut.getMessage(e);
 
-        // then
+        // Then
         assertThat(actual).isEqualTo(expected);
     }
 
     static Stream<Arguments> givenExceptionWithMessage_whenGettingMessage_thenReturnsMessage() {
         return Stream.of(
-                arguments(ErrorCode.OK, "OK - This is test message."),
+                arguments(ErrorCode.OK, "Ok - This is test message."),
                 arguments(ErrorCode.BAD_REQUEST, "Bad request - This is test message."),
                 arguments(ErrorCode.SPRING_BAD_REQUEST, "Spring-detected bad request - This is test message."),
                 arguments(ErrorCode.VALIDATION_ERROR, "Validation error - This is test message."),
@@ -40,17 +44,16 @@ class ErrorCodeTest {
         );
     }
 
-    @ParameterizedTest(name = "[{index}] \"{0}\" ===> \"{1}\"")
+    @DisplayName("에러 메시지를 받으면, 해당 에러 메시지로 출력")
     @MethodSource
-    @DisplayName("에러 메시지 받으면, 해당 메시지가 출력된다.")
+    @ParameterizedTest(name = "[{index}] \"{0}\" ===> \"{1}\"")
     void givenMessage_whenGettingMessage_thenReturnsMessage(String input, String expected) {
-        // given
-        Exception e = new Exception("This is test message.");
+        // Given
 
-        // when
+        // When
         String actual = ErrorCode.INTERNAL_ERROR.getMessage(input);
 
-        // then
+        // Then
         assertThat(actual).isEqualTo(expected);
     }
 
@@ -60,20 +63,66 @@ class ErrorCodeTest {
                 arguments("", ErrorCode.INTERNAL_ERROR.getMessage()),
                 arguments("   ", ErrorCode.INTERNAL_ERROR.getMessage()),
                 arguments("This is test message.", "This is test message.")
-
         );
     }
 
     @DisplayName("toString() 호출 포맷")
     @Test
     void givenErrorCode_whenToString_thenReturnsSimplifiedToString() {
-        // given
+        // Given
 
-        // when
+        // When
         String result = ErrorCode.INTERNAL_ERROR.toString();
 
-        // then
+        // Then
         assertThat(result).isEqualTo("INTERNAL_ERROR (20000)");
     }
 
+    @DisplayName("HttpStatus 에 대응하는 ErrorCode 찾기 - 정상")
+    @MethodSource
+    @ParameterizedTest(name = "[{index}] {0} ===> {1}")
+    void givenHttpStatus_whenGettingErrorCode_thenReturnsErrorCode(HttpStatus httpStatus, ErrorCode expected) {
+        // Given
+
+        // When
+        ErrorCode actual = ErrorCode.valueOf(httpStatus);
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> givenHttpStatus_whenGettingErrorCode_thenReturnsErrorCode() {
+        return Stream.of(
+                // 정의된 값
+                arguments(HttpStatus.OK, ErrorCode.OK),
+                arguments(HttpStatus.BAD_REQUEST, ErrorCode.BAD_REQUEST),
+                arguments(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.INTERNAL_ERROR),
+
+                // 정의되지 않은 값
+                arguments(HttpStatus.CONTINUE, ErrorCode.OK),
+                arguments(HttpStatus.ACCEPTED, ErrorCode.OK),
+                arguments(HttpStatus.MULTI_STATUS, ErrorCode.OK),
+                arguments(HttpStatus.MOVED_PERMANENTLY, ErrorCode.OK),
+                arguments(HttpStatus.CONFLICT, ErrorCode.BAD_REQUEST),
+                arguments(HttpStatus.EXPECTATION_FAILED, ErrorCode.BAD_REQUEST),
+                arguments(HttpStatus.BAD_GATEWAY, ErrorCode.INTERNAL_ERROR),
+                arguments(HttpStatus.BANDWIDTH_LIMIT_EXCEEDED, ErrorCode.INTERNAL_ERROR)
+        );
+    }
+
+    @DisplayName("HttpStatus 에 대응하는 ErrorCode 찾기 - Null 처리")
+    @Test
+    void givenUnknownHttpStatus_whenGettingErrorCode_thenReturnsErrorCode() {
+        // Given
+        HttpStatus nullStatus = null;
+
+        // When
+        Throwable t = catchThrowable(() -> ErrorCode.valueOf(nullStatus));
+
+        // Then
+        assertThat(t)
+                .isInstanceOf(GeneralException.class)
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.INTERNAL_ERROR)
+                .hasMessage("HttpStatus is null.");
+    }
 }
